6457bf334f057c7a31a549a79a68e8716f8234e3
Sina Pinto <sina.pinto@gmail.com>
2015-10-13 20:59:47 -0700

    Update styling and website layout

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -10,33 +10,33 @@ angular.module('mainCtrl', ['itemService'])
     $scope.selectedItemId = item.id;
   }
 
-	$scope.isLoading = true;
+  $scope.isLoading = true;
 
-	// grab all the items at page load
-	Item.all().success(function(data) {
-		$scope.isLoading = false;
-		$scope.items = data;
-	});
+  // grab all the items at page load
+  Item.all().success(function(data) {
+    $scope.isLoading = false;
+    $scope.items = data;
+  });
 
-	// function to delete a item
-	$scope.deleteItem = function(id) {
-		$scope.isLoading = true;
+  // function to delete a item
+  $scope.deleteItem = function(id) {
+    $scope.isLoading = true;
 
-		Item.delete(id).success(function(data) {
-			// get all items to update the table
-			Item.all().success(function(data) {
-				$scope.isLoading = false;
-				$scope.items = data;
-			});
+    Item.delete(id).success(function(data) {
+      // get all items to update the table
+      Item.all().success(function(data) {
+        $scope.isLoading = false;
+        $scope.items = data;
+      });
 
-		});
-	};
+    });
+  };
 
 })
 
 .controller('itemDetailController', function($scope, $routeParams, $http, Item) {
-	Item.get($routeParams.itemId).success(function(data) {
-		$scope.item = data[0];
-	});
+  Item.get($routeParams.itemId).success(function(data) {
+    $scope.item = data[0];
+  });
 
 });
