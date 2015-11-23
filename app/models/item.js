e60276c1a40b020052c25d11c94ac4cafce219d3
Logan <logan.masniuk@gmail.com>
2015-11-22 20:26:01 -0800

    Changed item Schema to reflect item hierarchy

--- app/models/item.js
+++ app/models/item.js
@@ -1,15 +1,55 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    extend = require('mongoose-schema-extend');
 var Schema = mongoose.Schema;
 
 var ItemSchema = new Schema({
   id: { type: Number, required: true },
   name: { type: String, required: true },
+  type: {type: String, required: true}.
   description: { type: String, required: false },
   quantity: { type: Number, required: false },
   location: { type: String, required: false },
-  brand: { type: String, required: false },
-  price: { type: String, required: false },
-  date_added: { type: Date, required: false }
+  brand: { type: String, required: false },  
+  date_added: { type: Date, required: false },
+  tags: {type: Array, required: false}
+});
+
+var ProductSchema = ItemSchema.extend({
+	price: { type: String, required: true }
+});
+
+var AdminEquipmentSchema = ItemSchema.extend({
+	techSupportContact: {type: String, required: true},
+	condition: { type: String, required: true }
+});
+
+
+var AnimalSchema = ProductSchema.extend({
+	color: { type: String, required: false},
+	breed: { type: String, required: false}
+});
+
+var AccessorySchema = ProductSchema.extend({
+	
+});
+
+var ConsumableSchema = ProductSchema.extend({
+	expiryDate: { type: Date, required: true},
+	breed: { type: String, required: false}
+});
+
+var ScannerSchema = AdminEquipmentSchema.extend({
+	dataFormat: {type: String, required: true}
+});
+
+var ComputerSchema = AdminEquipmentSchema.extend({
+	priveleges: {type: String, required: true}
+});
+
+var CardReaderSchema = AdminEquipmentSchema.extend({
+	bank: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Item', ItemSchema);
+module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('AdminEquipment', AdminEquipmentSchema);
