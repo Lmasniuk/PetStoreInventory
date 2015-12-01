2f5192ba048eb08fdb843a988b28dde59c9c8441
Sina Pinto <sina.pinto@gmail.com>
2015-11-30 18:45:43 -0800

    Revert "Revert "Merge pull request #6 from Lmasniuk/frances""
    This reverts commit e58a330d1faaf677d12df97885ecb91155e0ce25.

--- app/models/item.js
+++ app/models/item.js
@@ -14,7 +14,8 @@ var ItemSchema = new Schema({
   tags: {type: Array, required: false},
   price: { type: String, required: false },
   condition: {type: String, required: false},
-  contact: {type: String, required: false}
+  contact: {type: String, required: false},
+  state: {type: String, required: false}
 });
 
 /*
